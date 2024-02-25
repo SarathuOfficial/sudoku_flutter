fcaf38ca1fe93357e08e2c779d62f13d4a219827
Sarath Kumar P M <sarath1503kumargmail.com>
2024-02-25 14:59:34 +0530

    first commit

---
+++ lib/focusClass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FocusClass {
+  int? indexBox;
+  int? indexChar;
+
+  setData(int indexBox, int indexChar) {
+    this.indexBox = indexBox;
+    this.indexChar = indexChar;
+  }
+
+  focusOn(int indexBox, int indexChar) {
+    return this.indexBox == indexBox && this.indexChar == indexChar;
+  }
+}
